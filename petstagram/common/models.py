921a62218d0a0662fd24bf71935d23c7a40bff30
BuildTools <unconfigured@null.spigotmc.org>
2024-09-28 20:55:33 +0300

    feat: created models comment and like

--- petstagram/common/models.py
+++ petstagram/common/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 
+from petstagram.photos.models import Photo
 
-class
+
+class Comment(models.Model):
+    comment_text = models.TextField(
+        max_length=300,
+    )
+
+    datetime_of_publication = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+
+class Like(models.Model):
+    to_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
