4716554c00cfea1071321d4cd05413e999c0c575
bojkata1 <bojidar.enchev03@abv.bg>
2024-10-12 16:44:45 +0300

    feat: added add, edit and delete forms for the Pet model

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,19 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from petstagram.pets.models import Pet
-from petstagram.photos.models import Photo
+from petstagram.pets.forms import PetAddForm, PetEditForm, PetDeleteForm
 
 
 def pet_add_page(request):
-    return render(request, 'pets/pet-add-page.html')
+    form = PetAddForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('profile-details', 1)
+
+    context = {
+        'form': form
+    }
+    return render(request, 'pets/pet-add-page.html', context)
 
 
 def pet_edit_page(request, username: str, pet_slug: str):
-    return render(request, 'pets/pet-edit-page.html')
+    pet = Pet.objects.get(slug=pet_slug)
+    form = PetEditForm(request.POST or None, instance=pet)
+
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('pet-details', username, pet_slug)
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'pets/pet-edit-page.html', context)
 
 
 def pet_delete_page(request, username: str, pet_slug: str):
-    return render(request, 'pets/pet-delete-page.html')
+    pet = Pet.objects.get(slug=pet_slug)
+    form = PetDeleteForm(instance=pet)
+
+    if request.method == 'POST':
+        pet.delete()
+        return redirect('profile-details', pk=1)
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'pets/pet-delete-page.html', context)
 
 
 def pet_details_page(request, username: str, pet_slug: str):
